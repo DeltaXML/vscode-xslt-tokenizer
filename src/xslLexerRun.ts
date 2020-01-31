c5c70a95e449b38d23f037f1bf2df8b17283b250
Phil Fearon <phil.fearon@deltaxml.com>
2020-01-31 08:09:00 +0000

    remove xslDiagnostics.ts

--- src/xslLexerRun.ts
+++ src/xslLexerRun.ts
@@ -1,7 +1,7 @@
 // tslint:disable
 import { Token, TokenLevelState, ExitCondition, LexPosition } from "./xpLexer";
 import { XslLexer, XslToken } from "./xslLexer";
-import { XslDebug } from "./xslDiagnostics";
+
 import { Position } from "vscode";
 
 // -------------
@@ -28,35 +28,4 @@ lexer.flatten = flatten;
 lexer.timerOn = timerOnly;
 let pos: LexPosition = {line: 0, startCharacter: 0};
 let tokens: XslToken[]|Token[] = lexer.analyse(testXslt);
-
-
-if (generateTest) {
-	XslDebug.printMinSerializedTokens(testTitle, testXslt, tokens);
-} else if (timerOnly) {
-	console.log("XPath length: " + testXslt.length);
-	console.log("Token Count:" + tokens.length);
-} else {
-	console.log('---------------');
-	console.log(testXslt);
-	console.log('---------------');
-	XslDebug.printResultTokens(tokens);
-	if (flatten) {
-		console.log('===============');
-		printTokenValues(testXslt);
-	}
-}
-
-function printTokenValues(xpathExpr: string) {
-	let lines = xpathExpr.split(/\r\n|\r|\n/);
-	console.log('line count: ' + lines.length);
-	console.log('token count: ' + tokens.length);
-	for (let i = 0; i < tokens.length; i++) {
-		let t: Token|XslToken = tokens[i];
-		if (t.tokenType.valueOf() !== TokenLevelState.Whitespace.valueOf()) {
-			let line = lines[t.line];
-			let sc = t.startCharacter ? t.startCharacter : 0;
-			console.log(line.substr(sc, t.length) + '_');
-		}
-	}
-}
 
