b10d22614416e55dfc891f5c4c8f621f2b481baa
Phil Fearon <phil.fearon@deltaxml.com>
2020-03-13 07:32:38 +0000

    initial indents

--- src/xmlDocumentFormattingProvider.ts
+++ src/xmlDocumentFormattingProvider.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import {XslLexer, XMLCharState, XSLTokenLevelState} from './xslLexer';
-import {CharLevelState, TokenLevelState} from './xpLexer';
+import { XslLexer, XMLCharState, XSLTokenLevelState } from './xslLexer';
+import { CharLevelState, TokenLevelState } from './xpLexer';
 
 export class XMLDocumentFormattingProvider {
 	private xslLexer = new XslLexer();
@@ -16,14 +16,13 @@ export class XMLDocumentFormattingProvider {
 		console.log('formatter!!');
 		let indentString = '';
 		let useTabs = !(options.insertSpaces);
-		let spaceIndentSize = options.tabSize;
 		// using non-whitespace for testing only!!
 		if (useTabs) {
 			indentString = 't';
 		} else {
-			indentString = 's'.repeat(spaceIndentSize);
+			indentString = 's';
 		}
-		let indentCharLength = indentString.length;
+		let indentCharLength = useTabs? 1: options.tabSize;
 
 		let allTokens = this.xslLexer.analyse(document.getText());
 		let lineNumber = -1;
@@ -31,48 +30,47 @@ export class XMLDocumentFormattingProvider {
 		let stringLengthOffset = 0;
 		let xmlSpacePreserveStack: boolean[] = [];
 		allTokens.forEach((token) => {
-			if (token.line > lineNumber) {
-				lineNumber = token.line;
-				let actualIndentLength = token.startCharacter;
-				const currentLine = document.lineAt(lineNumber);
+			lineNumber = token.line;
+			let actualIndentLength = token.startCharacter;
+			const currentLine = document.lineAt(lineNumber);
 
-				let requiredIndentLength = nestingLevel * indentCharLength;
-				let isXsltToken = token.tokenType >= XMLDocumentFormattingProvider.xsltStartTokenNumber;
-				if (isXsltToken) {
-					let xmlCharType = <XMLCharState>token.charType;
-					let xmlTokenType = <XSLTokenLevelState>(token.tokenType -  XMLDocumentFormattingProvider.xsltStartTokenNumber);
-					switch (xmlTokenType) {
-						case XSLTokenLevelState.xmlPunctuation:
-							switch (xmlCharType) {
-								case XMLCharState.lSt:
-									nestingLevel++;
-									break;
-								case XMLCharState.rSelfCt:
-								case XMLCharState.lCt:
-									nestingLevel--;
-									break;
-							}
-							break;
-					}
-
-				} else {
-					let xpathCharType = <CharLevelState>token.charType;
-					let xpathTokenType = <TokenLevelState>token.tokenType;
+			let requiredIndentLength = nestingLevel * indentCharLength;
+			let isXsltToken = token.tokenType >= XMLDocumentFormattingProvider.xsltStartTokenNumber;
+			if (isXsltToken) {
+				let xmlCharType = <XMLCharState>token.charType;
+				let xmlTokenType = <XSLTokenLevelState>(token.tokenType - XMLDocumentFormattingProvider.xsltStartTokenNumber);
+				switch (xmlTokenType) {
+					case XSLTokenLevelState.xmlPunctuation:
+						switch (xmlCharType) {
+							case XMLCharState.lSt:
+								nestingLevel++;
+								break;
+							case XMLCharState.rSelfCt:
+							case XMLCharState.lCt:
+								nestingLevel--;
+								break;
+						}
+						break;
 				}
 
+			} else {
+				let xpathCharType = <CharLevelState>token.charType;
+				let xpathTokenType = <TokenLevelState>token.tokenType;
+			}
+			if (token.line > lineNumber) {
 				if (actualIndentLength !== requiredIndentLength) {
 					let indentLengthDiff = requiredIndentLength - actualIndentLength;
 
 					if (indentLengthDiff > 0) {
-						vscode.TextEdit.insert(currentLine.range.start, indentString.repeat(indentLengthDiff));
+						result.push(vscode.TextEdit.insert(currentLine.range.start, indentString.repeat(indentLengthDiff)));
 					} else {
 						let endPos = new vscode.Position(lineNumber, 0 - indentLengthDiff);
 						let deletionRange = currentLine.range.with(currentLine.range.start, endPos);
-						vscode.TextEdit.delete(deletionRange);
+						result.push(vscode.TextEdit.delete(deletionRange));
 					}
-				} 
-				lineNumber = token.line;
+				}
 			}
+			lineNumber = token.line;
 		});
 		return result;
 	}
