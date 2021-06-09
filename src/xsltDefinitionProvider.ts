cd3769e8af9c113a316187f83cc711c80f650e31
Phil Fearon <pgfearo@gmail.com>
2021-06-09 20:18:06 +0100

    added support for matching parent imports for auto-completion

--- src/xsltDefinitionProvider.ts
+++ src/xsltDefinitionProvider.ts
@@ -108,9 +108,10 @@ export class XsltDefinitionProvider implements vscode.DefinitionProvider, vscode
 		const xsltPackages: XsltPackage[] = <XsltPackage[]>vscode.workspace.getConfiguration('XSLT.resources').get('xsltPackages');
 
 		// Import/include XSLT - ensuring no duplicates
-		let importedG: ImportedGlobals = {data: globalInstructionData, href: document.fileName, error: false};
-		let importedGlobals1 = [importedG];
-		let accumulatedHrefs: string[] = [importedG.href];
+		const localImportedHrefs = XsltSymbolProvider.importSymbolHrefs;
+		let { importedGlobals1, accumulatedHrefs }:
+			{ importedGlobals1: ImportedGlobals[]; accumulatedHrefs: string[]; }
+			= await XsltSymbolProvider.processTopLevelImports(false, this.xslLexer, localImportedHrefs, document, globalInstructionData, xsltPackages);
 
 
 		let globalsSummary0: GlobalsSummary = {globals: importedGlobals1, hrefs: accumulatedHrefs};
