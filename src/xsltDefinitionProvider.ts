6113fa7acdbe08193c6467e46a08f5e96db0bb2a
Phil Fearon <pgfearo@gmail.com>
2021-06-12 07:18:43 +0100

    remove duplicated functions - use those declared in symbolprovider

--- src/xsltDefinitionProvider.ts
+++ src/xsltDefinitionProvider.ts
@@ -67,7 +67,7 @@ export class XsltDefinitionProvider implements vscode.DefinitionProvider, vscode
 		let processNestedGlobals = async () => {
 			let level = 0;
 			while (globalsSummary0.hrefs.length > 0 && level < maxImportLevel) {
-				globalsSummary0 = await this.processImportedGlobals(xsltPackages, globalsSummary0.globals, accumulatedHrefs, level === 0);
+				globalsSummary0 = await XsltSymbolProvider.processImportedGlobals(xsltPackages, globalsSummary0.globals, accumulatedHrefs, level === 0);
 				level++;
 			}
 		};
@@ -120,7 +120,7 @@ export class XsltDefinitionProvider implements vscode.DefinitionProvider, vscode
 		let processNestedGlobals = async () => {
 			let level = 0;
 			while (globalsSummary0.hrefs.length > 0 && level < maxImportLevel) {
-				globalsSummary0 = await this.processImportedGlobals(xsltPackages, globalsSummary0.globals, accumulatedHrefs, level === 0);
+				globalsSummary0 = await XsltSymbolProvider.processImportedGlobals(xsltPackages, globalsSummary0.globals, accumulatedHrefs, level === 0);
 				level++;
 			}
 		};
@@ -149,57 +149,5 @@ export class XsltDefinitionProvider implements vscode.DefinitionProvider, vscode
 			resolve(completions);
 		});
 
-	}
-
-	private async processImportedGlobals(xsltPackages: XsltPackage[], importedGlobals1: ImportedGlobals[], level1Hrefs: string[], topLevel: boolean): Promise<GlobalsSummary> {
-		let level2Globals: Promise<ImportedGlobals[]>[] = [];
-		let level2Hrefs = this.accumulateImportHrefs(xsltPackages, importedGlobals1, level1Hrefs);
-		let newGlobals: ImportedGlobals[] = [];
-
-		level2Hrefs.forEach((href) => {
-			level2Globals.push(XsltSymbolProvider.fetchImportedGlobals([href]));
-		});
-		let importedGlobals2Array = await Promise.all(level2Globals);
-		importedGlobals2Array.forEach((importedGlobals2) => {
-			if (topLevel) {
-				newGlobals = newGlobals.concat(importedGlobals2);
-			} else {
-				importedGlobals1 = importedGlobals1.concat(importedGlobals2);
-			}
-		});
-
-		if (topLevel) {
-			return {globals: newGlobals, hrefs: level2Hrefs};
-		} else {
-			return {globals: importedGlobals1, hrefs: level2Hrefs};
-		}
-	}
-
-	private accumulateImportHrefs(xsltPackages: XsltPackage[],importedGlobals: ImportedGlobals[], existingHrefs: string[]): string[] {
-		let result: string[] = [];
-		const rootPath = vscode.workspace.rootPath;
-		importedGlobals.forEach((importedG) => {
-			importedG.data.forEach((data) => {
-				if (data.type === GlobalInstructionType.Import || data.type === GlobalInstructionType.Include) {
-					let resolvedName = XsltSymbolProvider.resolvePath(data.name, importedG.href);
-					if (existingHrefs.indexOf(resolvedName) < 0) {
-						existingHrefs.push(resolvedName);
-						result.push(resolvedName);
-					}
-				} else if (rootPath && data.type === GlobalInstructionType.UsePackage) {
-					let packageLookup = xsltPackages.find((pkg) => {
-						return pkg.name === data.name;
-					});
-					if (packageLookup) {
-						let resolvedName = XsltSymbolProvider.resolvePathInSettings(packageLookup.path, rootPath);
-						if (existingHrefs.indexOf(resolvedName) < 0) {
-							existingHrefs.push(resolvedName);
-							result.push(resolvedName);
-						}
-					}
-				}
-			});
-		});
-		return result;
 	}
 }
