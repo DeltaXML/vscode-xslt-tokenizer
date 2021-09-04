47b6a96a2dbd1bc23b6583657bdbc87c33208caa
Phil Fearon <pgfearo@gmail.com>
2021-09-04 12:32:44 +0100

    hide console.log

--- src/xpathDocumentChangeHandler.ts
+++ src/xpathDocumentChangeHandler.ts
@@ -53,7 +53,7 @@ export class XPathDocumentChangeHandler {
 			doTrigger = precededByWS && followsSeparator;
 		}
 
-		console.log('doTrigger', doTrigger);
+		//console.log('doTrigger', doTrigger);
 
 		if (doTrigger) {
 			setTimeout(() => {
