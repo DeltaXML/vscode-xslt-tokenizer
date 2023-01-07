2e30b0981e3a6eccd5cd3de2d9cd5df749d2c054
Phil Fearon <phil.fearon@deltaxml.com>
2023-01-07 16:18:56 +0000

    recent files

--- src/fileSelection.ts
+++ src/fileSelection.ts
@@ -1,26 +1,54 @@
 import * as vscode from 'vscode';
 
 export class FileSelection {
+  private static fileList: string[] = ["one.xml", "two.xml"];
+  private static commandist: string[] = ["Pick File"];
   public static async pickFile(obj: { label: string; extensions?: string[] }) {
+
     const { label, extensions } = obj;
+    const fileItems = FileSelection.fileList.map(label => ({ label }));
+    const commandItems = FileSelection.commandist.map(label => ({ label }));
+    const OtherSeparator = {
+      label: 'command',
+      kind: vscode.QuickPickItemKind.Separator
+    };
+    const fileSeparator = {
+      label: 'recently used',
+      kind: vscode.QuickPickItemKind.Separator
+    };
+
+    const quickPick = vscode.window.createQuickPick();
+    let listItems: { label: string; kind?: vscode.QuickPickItemKind }[] = [];
+
+    listItems.push(fileSeparator);
+    listItems = listItems.concat(fileItems);
+    listItems.push(OtherSeparator);
+    listItems = listItems.concat(commandItems);
+    const qpOptions: vscode.QuickPickOptions = {
+      placeHolder: label
+    };
+    const picked = await vscode.window.showQuickPick(listItems, qpOptions);
+
+    quickPick.onDidHide(() => quickPick.dispose());
+    quickPick.show();
     let extensionFilters: { [key: string]: string[] } = {};
     if (extensions) {
       const filterLabel = `Extensions ${extensions.join(', ')}`;
       extensionFilters[filterLabel] = extensions;
     }
     const APP_FILE = await vscode.window.showOpenDialog({
-          title: label,
-          filters: extensionFilters,
-          canSelectFolders: false,
-          canSelectFiles: true,
-          canSelectMany: false,
-          openLabel: label,
-        });
-      
-        if (!APP_FILE || APP_FILE.length < 1) {
-          return;
-        } else {
-            return APP_FILE[0].fsPath;
-        }
-      }
+      title: label,
+      filters: extensionFilters,
+      canSelectFolders: false,
+      canSelectFiles: true,
+      canSelectMany: false,
+      openLabel: label,
+    });
+
+    if (!APP_FILE || APP_FILE.length < 1) {
+      return;
+    } else {
+      return APP_FILE[0].fsPath;
+    }
+  }
 }
