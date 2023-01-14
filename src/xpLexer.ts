3efea82e8d0c939a84f4a9f16cb3cc61f342c691
Phil Fearon <pgfearo@gmail.com>
2023-01-14 13:40:08 +0000

    treat atomic and type as types - allow all non-function-types before brackets

--- src/xpLexer.ts
+++ src/xpLexer.ts
@@ -144,7 +144,7 @@ export class Data {
     public static secondParts = ["as", "of"];
 
     public static nonFunctionConditional = ["if", "then", "else"];
-    public static nonFunctionTypes = ["map", "array", "function", "enum", "union", "record"];
+    public static nonFunctionTypes = ["atomic", "type", "map", "array", "function", "enum", "union", "record"];
     public static nonFunctionTypesBrackets = Data.nonFunctionTypes.map(t => t + '(*)');
 
     public static setAsOperatorIfKeyword(token: Token) {
@@ -890,7 +890,7 @@ export class XPathLexer {
                 prevToken.tokenType = TokenLevelState.complexExpression;
             } else if (prevToken.value === 'function') {
                 prevToken.tokenType = TokenLevelState.anonymousFunction;
-            } else if (prevToken.value === 'map' || prevToken.value === 'array') {
+            } else if (Data.nonFunctionTypes.includes(prevToken.value)) {
                 prevToken.tokenType = TokenLevelState.simpleType;
             } else {
                 prevToken.tokenType = TokenLevelState.function;
