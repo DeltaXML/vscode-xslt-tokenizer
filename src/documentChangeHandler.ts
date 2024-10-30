1e274cbd138e0bcc630d2f3d152d804da0b9d91f
Phil Fearon <phil.fearon@deltaxml.com>
2024-10-30 15:36:25 +0000

    extensiion did not allow tag-rename on copy/paste of text greated than 2 chars in length!

--- src/documentChangeHandler.ts
+++ src/documentChangeHandler.ts
@@ -44,7 +44,7 @@ export class DocumentChangeHandler {
 		}
 		let activeChange = e.contentChanges[0];
 		let skipTrigger = false;
-		if (!activeChange || (activeChange.text.length > 2 && !activeChange.text.endsWith('::'))) {
+		if (!activeChange || (activeChange.text.length > 20 && !activeChange.text.endsWith('::'))) {
 			return;
 		}
 		if (activeChange.text === '/' && activeChange.rangeLength === 0) {
