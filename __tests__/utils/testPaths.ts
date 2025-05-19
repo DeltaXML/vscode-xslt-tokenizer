b6d11903c4c6242c8afa815737239f4207d89632
Phil Fearon <phil.fearon@deltaxml.com>
2025-05-19 09:29:56 +0100

    use constant for xsl data dir path

--- __tests__/utils/testPaths.ts
+++ __tests__/utils/testPaths.ts
@@ -3,4 +3,5 @@ import * as path from 'path';
 export class TestPaths {
     public static testDir = "__tests__";
     public static testDataDir = path.join(this.testDir, "data");
+    public static testXslDataDir = path.join(this.testDataDir, "xsl-test-files");
 }
