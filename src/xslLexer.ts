1a28d1f79adc9ba27cdc401ee05899e621193674
Phil Fearon <phil.fearon@deltaxignia.com>
2025-07-11 15:47:14 +0100

    IMPORTANT: lexer bug - don't add pi token twice

--- src/xslLexer.ts
+++ src/xslLexer.ts
@@ -729,7 +729,6 @@ export class XslLexer {
                         switch (nextState) {
                             case XMLCharState.lPiValue:
                                 addToken = XSLTokenLevelState.processingInstrValue;
-                                this.addNewTokenToResult(tokenStartChar, addToken, result, nextState);
                                 break;
                             case XMLCharState.lComment:
                                 addToken = XSLTokenLevelState.xmlComment;
